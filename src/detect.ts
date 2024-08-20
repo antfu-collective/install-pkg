d513fb2af2efce9e556a95e17d283edb24a8970e
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-08-20 13:22:50 -0700

    feat: switch to `package-manager-detector` (#13)
    Co-authored-by: Anthony Fu <github@antfu.me>

--- src/detect.ts
+++ src/detect.ts
@@ -1,52 +1,17 @@
-import fs from 'node:fs'
-import path from 'node:path'
 import process from 'node:process'
-import { findUp } from 'find-up'
+import { type Agent, detect } from 'package-manager-detector'
+
+export { type Agent } from 'package-manager-detector'
 
 export type PackageManager = 'pnpm' | 'yarn' | 'npm' | 'bun'
 
-const AGENTS = ['pnpm', 'yarn', 'npm', 'pnpm@6', 'yarn@berry', 'bun'] as const
-export type Agent = typeof AGENTS[number]
-
-const LOCKS: Record<string, PackageManager> = {
-  'bun.lockb': 'bun',
-  'pnpm-lock.yaml': 'pnpm',
-  'yarn.lock': 'yarn',
-  'package-lock.json': 'npm',
-  'npm-shrinkwrap.json': 'npm',
-}
-
 export async function detectPackageManager(cwd = process.cwd()): Promise<Agent | null> {
-  let agent: Agent | null = null
-  const lockPath = await findUp(Object.keys(LOCKS), { cwd })
-  let packageJsonPath: string | undefined
+  const result = await detect({
+    cwd,
+    onUnknown(packageManager) {
+      console.warn('[@antfu/install-pkg] Unknown packageManager:', packageManager)
+    },
+  })
 
-  if (lockPath)
-    packageJsonPath = path.resolve(lockPath, '../package.json')
-  else
-    packageJsonPath = await findUp('package.json', { cwd })
-
-  if (packageJsonPath && fs.existsSync(packageJsonPath)) {
-    try {
-      const pkg = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
-      if (typeof pkg.packageManager === 'string') {
-        const [name, version] = pkg.packageManager.split('@')
-        if (name === 'yarn' && Number.parseInt(version) > 1)
-          agent = 'yarn@berry'
-        else if (name === 'pnpm' && Number.parseInt(version) < 7)
-          agent = 'pnpm@6'
-        else if (AGENTS.includes(name))
-          agent = name
-        else
-          console.warn('[ni] Unknown packageManager:', pkg.packageManager)
-      }
-    }
-    catch {}
-  }
-
-  // detect based on lock
-  if (!agent && lockPath)
-    agent = LOCKS[path.basename(lockPath)]
-
-  return agent
+  return result?.agent || null
 }
